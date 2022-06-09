9471b3d55178870f04aefae7e4f592def43fb8c5
AdeshMask <adeshmaske255@gmail.com>
2022-06-09 11:56:31 +0530

    Added:Event On click Logo

--- src/App.js
+++ src/App.js
@@ -3,17 +3,27 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+
+  url = 'https://github.com/AdeshMask'
   constructor() {
     super()
     this.state ={
       title: 'Hello From Bridgelabz'
     }
   }
+
+  //onClick Function
+  onClick = ($event) => {
+    console.log('Save button is clicked !', $event);
+    window.open(this.url, "_blank");
+  }
+
   render(){
     return (
       <div>
         <h1>{this.state.title}</h1>
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} onClick={this.onClick} 
+        className="App-logo" alt="logo" />
       </div>
     )
   }
