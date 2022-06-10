242d7ea0414fa48e2b1f4a7c931d70e039de45d3
AdeshMask <adeshmaske255@gmail.com>
2022-06-10 10:22:10 +0530

    Added:React-router-dom

--- src/App.js
+++ src/App.js
@@ -1,60 +1,19 @@
-import logo from './assests/bridgelabz-logo.png';
-import React from 'react';
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
+import Home from './Components/Home'
 
-class App extends React.Component {
-
-  url = 'https://www.google.com'
-  constructor() {
-    super()
-    this.state ={
-      userName: '',
-      nameError:''
-    }
-  }
-
-  //onClick Function
-  onClick = ($event) => {
-    console.log('Save button is clicked !', $event);
-    window.open(this.url, "_blank");
-  }
-
-  onNameChange = (event) => {
-    console.log('value is', event.target.value);
-    const nameRegex = RegExp("^[A-Z]{1}[a-z]{2,}$");
-    this.setState({userName: event.target.value});
-    if(nameRegex.test(event.target.value)){
-      this.setState({nameError:''})
-    }
-    else this.setState({nameError:'Incorrect name'})
-  }
-
-  render(){
-    return (
-      <>
-      <div className="App">
-        <h1>Hello {this.state.userName} From Bridgelabz</h1>
-        <img src={logo} onClick={this.onClick} 
-        className="App-logo" alt="logo" />
-      </div>
-      <div className="App">
-        <br/><input onChange={this.onNameChange}/>
-        <span className="error-output">{this.state.nameError}</span>
-        </div>
-        <p>At Bridgelabz we are a Community of</p>
-	  <ul>
-	    <li>Technologists</li>
-	    <li>Thinkers</li> 
-	    <li>Builders</li> 
-     </ul>
-       <p>Working together to keep the employability of Engineers alive and accessible, so Tech Companies worldwide can get contributors and creators for technology Solutions. We belive this act of human collaboration across an employability platfor is essential to individual growth and our collevtive future.</p>
-			 <p>To know more about us, visit <a href="https://www.bridgelabz.com/">Bridgelabz</a> to learn even more about out mission</p>
-	     <p><strong>i.e Employability to all</strong></p>
-			 
-      </>
-    )
-  }
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+      <Routes>
+            <Route path="/home" element={<Home />} />
+           
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
-
 
 export default App;
